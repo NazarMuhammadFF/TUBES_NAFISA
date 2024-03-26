21af30bc58cde2050fc2d7ad4e3bd241a5d0a733
fril08 <safrilyusupmaulana27@gmail.com>
2024-03-26 19:17:17 +0700

    halaman hero

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ var tombolMenu = $(".tombol-menu");
 var menu = $("nav .menu ul");
 
 function klikMenu(){
-   tombolMenu.clik(function(){
+   tombolMenu.click(function(){
        menu.toggle();
     });
-    menu.clik(function(){
+    menu.click(function(){
        menu.toggle();
     });
 }
-$(document).ready(function(){
+
+$(document).ready(function() {
     var width = $(window).width();
     if(width < 990){
         klikMenu();
